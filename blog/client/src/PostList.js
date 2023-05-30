ddfdc980e2cb41fd890b6a9ee2ddf48cc6a9dbf7
root <root@GARUDA.localdomain>
2023-05-30 17:52:41 +0530

    Post list created

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -18,13 +18,13 @@ const PostList = () => {
   const renderedPosts = Object.values(posts).map((post) => {
     return (
       <div
-        className='card'
+        className="card"
         style={{
           width: "30%",
           marginBottom: "20px",
         }}
         key={post.id}>
-        <div className='card-body'>
+        <div className="card-body">
           <h3>{post.title}</h3>
 
           <CommentList comments={post.comments} />
@@ -35,7 +35,11 @@ const PostList = () => {
     );
   });
 
-  return <div className='d-flex flex-row flex-wrap justify-content-between'>{renderedPosts}</div>;
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 };
 
 export default PostList;
